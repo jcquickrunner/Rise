2f9b99b47dd49afe868582609bad2d0d637333b3
Juan <juan@seasonmint.com>
2020-04-06 19:27:28 -0400

    "add states to different actions and introduced climbing"

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,10 +6,16 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour
 {
     //--------------CONFIG---------------
+
+    enum MethodController { Climbing, Running, Nothing, jump };
+
     [SerializeField] float runSpeed = 5f;
-    [SerializeField] float jumpSpeed= 4f;
+    [SerializeField] float jumpSpeed = 4f;
+    [SerializeField] float ClimbSpeed = 3f;
     //-----------------------STATE---------------------------
-bool isAlive = true;
+    bool isAlive = true;
+    MethodController currentMethod = MethodController.Nothing;
+
     //------------------------CACHE------------------------------
     Rigidbody2D myRigidBody;
     Animator myAnimator;
@@ -28,25 +34,74 @@ bool isAlive = true;
         run();
         FlipSprite();
         Jump();
-    
+        ClimbLadder();
+
     }
     private void run()
     {
-        float controlFlow = CrossPlatformInputManager.GetAxis("Horizontal");
-        Vector2 playerVelocity = new Vector2(controlFlow * runSpeed, myRigidBody.velocity.y);// velocity has an x and y
-        myRigidBody.velocity = playerVelocity;// sets the velociy for player
-        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;// controls set in the input manager in unity
-       
-        myAnimator.SetBool("Run",playerHasHorizontalSpeed);//second parameter looks for true or false-- this animation"run" is true or false
-        
-    }
-    private void Jump(){
-        if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("ground"))) { return;}// if not touching ground layer then run the rest of jump method
-        if(CrossPlatformInputManager.GetButtonDown("Jump")){
-        Vector2 jumpVelocityToAdd = new Vector2 (0f, jumpSpeed);
-        myRigidBody.velocity += jumpVelocityToAdd;// the x and y of velocity to add
+        currentMethod = MethodController.Running;
+        if (currentMethod == MethodController.Running)
+        {
+            float controlFlow = CrossPlatformInputManager.GetAxis("Horizontal");
+            Vector2 playerVelocity = new Vector2(controlFlow * runSpeed, myRigidBody.velocity.y);// velocity has an x and y
+            myRigidBody.velocity = playerVelocity;// sets the velociy for player
+            bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;// controls set in the input manager in unity
+
+            myAnimator.SetBool("Run", playerHasHorizontalSpeed);//second parameter looks for true or false-- this animation"run" is true or false
+
         }
-        
+
+
+    }
+    private void Jump()
+    {
+        // currentMethod = MethodController.jump;
+        if (currentMethod == MethodController.Running)
+        {
+            if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("ground"))) { return; }// if not touching ground layer then run the rest of jump method
+            if (CrossPlatformInputManager.GetButtonDown("Jump"))
+            {
+                Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+                myRigidBody.velocity += jumpVelocityToAdd;// the x and y of velocity to add
+            }
+
+        }
+
+
+    }
+
+    private void ClimbLadder()
+    { //THIS COULD BE ALTERED SO THAT THE CLIMB LADDER METHOD IS ONLY AVAILABLE IF NOT TOUCHING GROUND LAYER
+        currentMethod = MethodController.Climbing;
+        if (currentMethod == MethodController.Climbing)
+        {
+            if (myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ladder"))) //does the opposite actually of what it reads if you put a return after it
+            {
+                float controlFlow = CrossPlatformInputManager.GetAxis("Vertical");// lets you use both buttons already w and s
+                Vector2 playerClimbVelocity = new Vector2(myRigidBody.velocity.x, controlFlow * ClimbSpeed);
+                myRigidBody.velocity = playerClimbVelocity;
+                bool playerHasverticalSpeed = Mathf.Abs(myRigidBody.velocity.y) > Mathf.Epsilon || myCollider2D.IsTouchingLayers(LayerMask.GetMask("ground"));
+                myAnimator.SetBool("Climb", playerHasverticalSpeed);
+                //  Vector2 climbVelocityToAdd = new Vector2(0f,ClimbSpeed); //x and y climb velocity
+                //  myRigidBody.velocity += climbVelocityToAdd; // add this velocity on x and y
+            }
+            // else if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("ground")))
+            // {
+            //     myAnimator.SetBool("Climb", false);
+            // }
+            else if (myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ladder")))
+            {
+                bool playerHasverticalSpeed = Mathf.Abs(myRigidBody.velocity.y) > Mathf.Epsilon || myCollider2D.IsTouchingLayers(LayerMask.GetMask("ground"));
+                myAnimator.SetBool("Climb", playerHasverticalSpeed);
+            }
+            else if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ladder")))
+            {
+                Vector2 velocityWhileClimbing = new Vector2(0f, myRigidBody.velocity.y);
+                myAnimator.SetBool("Climb", false);
+            }
+
+        }
+
     }
     private void FlipSprite()
     { // turn player around
